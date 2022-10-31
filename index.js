acc30921e1b2a169c0c5c842ba45039958f90808
yerlinbenavides <yerlinvanessabenavides2408@gmail.com>
2022-10-31 18:07:34 -0500

    commit N 1

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+
+//import blogRoutes from './routes/routes.js'
+
+const app = express ()
+app.use(cors())
+app.use(express.json())
+//app.use('/usuarios',blogRoutes)
+
+app.get("/", (req,res)=>{
+    res.send(" HOLA MUNDO")
+})
+
+app.listen(5000, ()=>{
+    console.log('Servidor Corriendo en http://localhost:5000/')
+})
